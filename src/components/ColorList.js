6e9ece07893c516b13f8b6bbde4559f97fc99d53
waltersimekha <waltersimekha@gmail.com>
2024-02-11 02:15:26 +0300

    complete assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -8,17 +8,13 @@ function ColorList() {
     "darkslategray",
     "hotpink",
   ];
-
+  const colorList=colors.map((color)=><li key={color} style={{color:color}}>{color}</li>)
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
-      </ol>
+        {colorList}
+        </ol>
     </div>
   );
 }
